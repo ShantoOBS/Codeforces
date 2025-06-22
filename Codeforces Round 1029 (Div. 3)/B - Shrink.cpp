6f347eb730fd8e7e1a1774e41400645c0ac30d50
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2025-06-22 11:23:15 +0600

    Create B - Shrink.cpp

---
+++ Codeforces Round 1029 (Div. 3)/B - Shrink.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+void solve() {
+    int n; cin>>n;
+    
+    cout<<1<<" ";
+    for(int i=3;i<=n;i++)cout<<i<<" ";
+    cout<<2<<endl;
+    
+    //cout<<endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) solve();
+}
