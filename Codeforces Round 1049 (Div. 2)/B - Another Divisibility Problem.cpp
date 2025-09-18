4942b221c716feb28efdcd23b4589ee49cb2c268
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2025-09-18 08:54:56 +0600

    Create B - Another Divisibility Problem.cpp

---
+++ Codeforces Round 1049 (Div. 2)/B - Another Divisibility Problem.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastIO                                                                 \
+    ios::sync_with_stdio(false);                                               \
+    cin.tie(NULL);                                                             \
+    cout.tie(NULL);                                                            \
+    cout.precision(numeric_limits<double>::max_digits10);
+
+#define int long long
+#define all(v) v.begin(),v.end()
+#define vl vector<int>
+#define pb emplace_back
+#define in(v) for(auto &k:v)cin>>k;
+
+
+
+inline void solve(){
+    
+  int n; cin>>n;
+  
+  cout<<1000000000-1-n<<endl;
+  
+  
+  
+
+} 
+
+// tc :
+// sc :
+
+int32_t main() {
+    fastIO;
+    
+    int t; cin>>t;
+    while(t--)solve();
+
+    return 0;
+}
